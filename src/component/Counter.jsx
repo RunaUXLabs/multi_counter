5c7f598c56770b40cc69c63ff2496bc087ca259e
Runa <runa.uxlabs@gmail.com>
2025-08-19 14:51:58 +0900

    prop-types 관련 eslint 규칙 비활성화 주석 제거

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 function Counter({ index, count, onIncrease, onDecrease }) {
   return (
     <div className="counter">
--- src/component/Total.jsx
+++ src/component/Total.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 function Total({ counters }) {
   const total = counters.reduce((sum, count) => sum + count, 0);
 
